b6995b8dbb5f37b7c9c9722605f38d4e8b73248a
root <root@sandbox.(none)>
2013-06-27 13:40:33 -0700

    script works

--- install-rstudio.sh
+++ install-rstudio.sh
@@ -24,13 +24,13 @@ cat /etc/profile.d/hadoop.sh | sed 's/export //g' > ~/.Renviron
 
 # Download and install RStudio server
 wget -O /tmp/rstudio-server-0.97.332-x86_64.rpm http://download2.rstudio.org/rstudio-server-0.97.332-x86_64.rpm
-yum -y install -nogpgcheck /tmp/rstudio-server-0.97.332-x86_64.rpm
+yum -y install --nogpgcheck /tmp/rstudio-server-0.97.332-x86_64.rpm
 
 # Run commands in R to install libs needed for RMR2 (?)
 R CMD BATCH install_packages.r
 
 # Get rmr2
-wget -O /tmp/rmr2_2.1.0.tar.gz https://github.com/RevolutionAnalytics/rmr2/blob/master/build/rmr2_2.1.0.tar.gz
+wget -O /tmp/rmr2_2.1.0.tar.gz https://github.com/RevolutionAnalytics/rmr2/blob/master/build/rmr2_2.1.0.tar.gz?raw=true
 
 # Install RMR2
 R CMD INSTALL /tmp/rmr2_2.1.0.tar.gz
